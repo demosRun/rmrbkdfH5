cab88f30926a1a425cef08def765ecd372b31513
puge <mail@puge.org>
2020-09-24 23:35:45 +0800

    Initial commit

---
+++ dist/static/js/main.js
@@ -0,0 +1,25 @@
+function scrollIntoView () {
+  window.scrollTo(0, 0)
+}
+
+// 阻止微信拖动
+document.body.addEventListener('touchmove', function (e) {
+  e.preventDefault() // 阻止默认的处理方式(阻止下拉滑动的效果)
+}, {passive: false})
+
+window.onload = function(){
+  autoScale({
+    deviseW: 750,
+    deviseH: 1508,
+    center: 'middle',
+    scroll: false,
+    type: 'scale',
+    box: '.scale-box'
+  })
+}
+
+function showInfo () {
+  console.log('sd')
+  // go('page3//rotatePushTop/rotatePullBottom&&ontop/true/rotatePushBottom/rotatePullTop&&ontop')
+  owo.go('page3//rotatePushTop/rotatePullBottom&&ontop/true/rotatePushBottom/rotatePullTop&&ontop')
+}
